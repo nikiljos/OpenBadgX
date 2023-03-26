bc06f613ba596c38397ba4d6feb7d30aad2c098c
Nikhil Jose <nikhiljose12345@gmail.com>
2023-03-26 19:41:33 +0530

    chore: update tempalte migration script to accept tempalte name as arg

--- backend/src/scripts/syncTemplate.ts
+++ backend/src/scripts/syncTemplate.ts
@@ -4,42 +4,59 @@ dotenv.config();
 import { syncTemplate } from "../utils/ses";
 import { readFile } from "fs";
 import { promisify } from "util";
+const fileRead = promisify(readFile);
 
 (async () => {
-    console.log("=====Starting Template Migration=====")
-    console.log("=====================================\n");
-    const fileRead = promisify(readFile);
+
+    const templateName=process.argv[2]
+    if (!templateName || templateName === "" || !/^[A-Za-z0-9]*$/.test(templateName)) {
+        console.log("Invalid Template Name");
+        return;
+    }
+    console.log("Template:",templateName,"\n")
+
+    console.log("=====================================");
+    console.log("=====Starting Template Migration=====\n");
+    
     const htmlData = await fileRead(
-        "./src/assets/mailTemplate/assertionNotify.html"
-    ).catch((err) => console.log("file read error:", err));
-    if (!htmlData) return;
+        `./src/assets/mailTemplate/${templateName}-template.html`
+    ).catch((err) => console.log("Template file read error:", err.message));
+    const subData = await fileRead(
+        `./src/assets/mailTemplate/${templateName}-subject.txt`
+    ).catch((err) => console.log("Subject file read error:", err.message));
+    if (!htmlData||!subData) return;
     const htmlString = htmlData.toString();
-    console.log("HTML to migrate-->\n", htmlString,"\n\n");
+    const subject=subData.toString()
+    console.log("HTML to migrate ↓\n", htmlString, "\n");
+    console.log("Subject to migrate →", subject, "\n\n");
     let sesResponse;
     let existStatus = false;
     sesResponse = await syncTemplate(
         true,
-        "assertionNotify",
-        "Congrats, You have recieved a badge from {{orgName}}",
+        templateName,
+        subject,
         htmlString
     ).catch((err) => {
-        if (err.Error.Code === "AlreadyExists") existStatus = true;
+        if (err.Error.Code === "AlreadyExists"){
+            existStatus = true;
+            console.log("Template Already Exists! Updating...\n")
+        } 
         else console.log(err);
     });
     if (sesResponse) console.log("Create Successful");
     else if (existStatus) {
         sesResponse = await syncTemplate(
             false,
-            "assertionNotify",
-            "Congrats, You have recieved a badge from {{orgName}}",
+            templateName,
+            subject,
             htmlString
         ).catch((err) => {
             console.log(err);
         });
         if (sesResponse) console.log("Update Successful");
     }
-    console.log(sesResponse,"\n");
+    // console.log(sesResponse,"\n");
 
+    console.log("\n=====Template Migration Finished=====");
     console.log("=====================================");
-    console.log("=====Template Migration Finished=====");
 })();
